bd8db736d7143cbea25653d78245efbfcbade0d8
zengzhu <zeng.zhu@gmail.com>
2015-10-05 12:22:51 +0800

    add api.

---
+++ android/kreader/src/com/onyx/reader/plugin/ReaderDocumentOptions.java
@@ -0,0 +1,16 @@
+package com.onyx.reader.plugin;
+
+import java.util.List;
+
+/**
+ * Created by zhuzeng on 10/2/15.
+ * Defined in host and used by plugin.
+ */
+public interface ReaderDocumentOptions {
+
+    public String getPassword();
+
+    public String getCompressedPassword();
+
+
+}
